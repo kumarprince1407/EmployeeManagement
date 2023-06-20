5c5b9d470144e4b440cbc32914678e8dc0b2e562
Prince Kumar <kumarprince1407@gmail.com>
2023-06-20 23:12:48 +0530

    Added Use Case 1

--- EmployeeManagement/src/com/bridgelabz/employeemanagement/EmployeeManagement.java
+++ EmployeeManagement/src/com/bridgelabz/employeemanagement/EmployeeManagement.java
@@ -4,6 +4,14 @@ import java.util.*;
 public class EmployeeManagement {
     public static void main(String args[]){
         System.out.println("Welcome to Employee Wage Computation Program.");
-       
+        Random random = new Random(2);
+        int attendance = random.nextInt(2);
+
+        if(attendance==1){
+            System.out.println("Employee is present.");
+        }
+        else{
+            System.out.println("Employee is absent.");
+        }
     }
 }
