3edd32e2ad2e24138af07332ec4843d17963da17
Prince Kumar <kumarprince1407@gmail.com>
2023-06-20 22:35:27 +0530

    Added a Use Case to Master branch

---
+++ EmployeeManagement/src/com/bridgelabz/employeemanagement/EmployeeManagement.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeemanagement;
+
+import java.util.*;
+public class EmployeeManagement {
+    public static void main(String args[]){
+        System.out.println("Welcome to Employee Wage Computation Program.");
+       
+    }
+}
