9281ebd40d2c909015ac35bcb32336a8546f041f
Prince Kumar <kumarprince1407@gmail.com>
2023-06-21 03:00:12 +0530

    Added Use Case 5

--- EmployeeManagement/src/com/bridgelabz/employeemanagement/EmployeeManagement.java
+++ EmployeeManagement/src/com/bridgelabz/employeemanagement/EmployeeManagement.java
@@ -4,14 +4,36 @@ import java.util.*;
 public class EmployeeManagement {
     public static void main(String args[]){
         System.out.println("Welcome to Employee Wage Computation Program.");
-        Random random = new Random(2);
-        int attendance = random.nextInt(2);
+        Random random = new Random();
+        //int attendance = random.nextInt(3);
+        int empWagePerHr = 20;
+        int fullDayHr = 8;
+        int partTimeHour = 4;
+        int totalWage = 0;
+        int workingDaysPerMonth=20;
+        for (int day = 1; day <= workingDaysPerMonth; day++) {
+            int attendance = random.nextInt(3);
 
-        if(attendance==1){
-            System.out.println("Employee is present.");
+            int dailyWage = 0;
+            String attendanceType = "";
+            switch (attendance) {
+                case 1:
+                    dailyWage = empWagePerHr * fullDayHr;
+                    attendanceType = "full-time";
+                    break;
+                case 2:
+                    dailyWage = empWagePerHr * partTimeHour;
+                    attendanceType = "part time";
+                    break;
+                default:
+                    attendanceType = "absent";
+            }
+            System.out.println("Employee is "+ attendanceType +". Daily Wage: "+ dailyWage);
+            totalWage += dailyWage;
         }
-        else{
-            System.out.println("Employee is absent.");
-        }
+        System.out.println("Total wage of the employee "+ totalWage);
     }
+
+
+
 }
