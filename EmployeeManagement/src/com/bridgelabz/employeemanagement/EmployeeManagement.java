c4245fc1e4707ff31d71bd44ce2555e61f3a0dec
Prince Kumar <kumarprince1407@gmail.com>
2023-06-21 03:16:44 +0530

    Added Use Case 6

--- EmployeeManagement/src/com/bridgelabz/employeemanagement/EmployeeManagement.java
+++ EmployeeManagement/src/com/bridgelabz/employeemanagement/EmployeeManagement.java
@@ -4,14 +4,43 @@ import java.util.*;
 public class EmployeeManagement {
     public static void main(String args[]){
         System.out.println("Welcome to Employee Wage Computation Program.");
-        Random random = new Random(2);
-        int attendance = random.nextInt(2);
+        Random random = new Random();
+        //int attendance = random.nextInt(3);
+        int empWagePerHr = 20;
+        int fullDayHr = 8;
+        int partTimeHour = 4;
+        int totalWage = 0;
+       // int workingDaysPerMonth=20;
+        int totalWorkingHours=0;
+        int maxWorkingHours=100;
+        int totalWorkingDays=0;
+        int maxWorkingDays=20;
 
-        if(attendance==1){
-            System.out.println("Employee is present.");
+        while (totalWorkingHours <= maxWorkingHours && totalWorkingDays <= maxWorkingDays) {
+            int attendance = random.nextInt(3);
+
+            int dailyWage = 0;
+            int dailyHours=0;
+            String attendanceType = "";
+            switch (attendance) {
+                case 1:
+                    dailyWage = empWagePerHr * fullDayHr;
+                    attendanceType = "full-time";
+                    break;
+                case 2:
+                    dailyWage = empWagePerHr * partTimeHour;
+                    attendanceType = "part time";
+                    break;
+                default:
+                    attendanceType = "absent";
+            }
+            System.out.println("Employee is "+ attendanceType +". Daily Wage: "+ dailyWage);
+            totalWorkingDays++;
+            totalWage += dailyWage;
         }
-        else{
-            System.out.println("Employee is absent.");
-        }
+        System.out.println("Employee was present for " + totalWorkingDays+" days, and the total wage of the employee is "+ totalWage+".");
     }
+
+
+
 }
